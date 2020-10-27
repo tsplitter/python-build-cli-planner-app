f931091260fb46f63028f3dbbecf3d32696cc7a5
tsplitter <pa.tsplitter@yahoo.com>
2020-10-27 23:05:56 +0000

    Completed module

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
